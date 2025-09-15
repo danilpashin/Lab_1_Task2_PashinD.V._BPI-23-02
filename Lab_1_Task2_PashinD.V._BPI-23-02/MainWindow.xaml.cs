5f9faae74b665429fbb0d2ed96f001490040a73c
Xoriarty <xoriartycode@gmail.com>
2025-09-15 17:51:36 +0500

    Добавлен метод площади в интерфейс

--- Lab_1_Task2_PashinD.V._BPI-23-02/MainWindow.xaml.cs
+++ Lab_1_Task2_PashinD.V._BPI-23-02/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Lab_1_Task2_PashinD.V._BPI_23_02
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Circle circleA;
+        public Rectangle rectangleA;
+        public Triangle triangleA;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void getArea(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            switch(pressed.Content.ToString())
+            {
+                case "Круг":
+                    CircleData.Visibility = Visibility.Hidden;
+                    RectangleData.Visibility = Visibility.Hidden;
+                    TriangleData.Visibility = Visibility.Visible;
+                    break;
+                case "Прямоугольник":
+                    break;
+                case "Треугольник":
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
